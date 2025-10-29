43d2616c2b678442d87e05781b80a2a869297b20
nhut-nam <namnhut1426@gmail.com>
2025-10-29 19:52:43 +0700

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Nhut Nam",
+    author_email="namnhut1426@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
