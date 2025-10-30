75629d880804a70cbc7eec654edd28d3242808a4
nhut-nam <namnhut1426@gmail.com>
2025-10-30 19:00:08 +0700

    store_vector added

---
+++ src/helpers.py
@@ -0,0 +1,31 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from typing import List
+from langchain.schema import Document
+
+def load_pdf_files(folder_path):
+    loader = DirectoryLoader(folder_path, glob="*.pdf", loader_cls=PyPDFLoader)
+    documents = loader.load()
+    return documents
+
+def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
+    minimal_docs: List[Document] = []
+    for doc in docs:
+        src = doc.metadata.get("source")
+        minimal_doc = Document(page_content=doc.page_content, metadata={"source": src})
+        minimal_docs.append(minimal_doc)
+    return minimal_docs
+
+def text_split(minimal_docs):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+    split_docs = text_splitter.split_documents(minimal_docs)
+    return split_docs
+
+def download_embeddings():
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    embeddings = HuggingFaceEmbeddings(model_name=model_name)
+    return embeddings
---
+++ src/prompt.py
@@ -0,0 +1,5 @@
+system_prompt = (
+    "You are a helpful medical assistant. Use the following context to answer the question.\n"
+    "If you don't know the answer, just say you don't know. Do not try to make up an answer.\n"
+    "{context}"
+)
